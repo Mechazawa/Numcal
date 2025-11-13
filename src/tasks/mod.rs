db4947b157533839cba926c3d086815e71defcca
Bas Bieling <bas@scrumble.nl>
2025-11-13 21:25:25 +0100

    Watchdog task

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -2,6 +2,7 @@ mod usb;
 mod display;
 mod keypad;
 mod hotkeys;
+mod watchdog;
 
 pub use display::init as init_display;
 pub use display::DisplayProxy;
@@ -10,3 +11,4 @@ pub use keypad::init as init_keypad;
 pub use keypad::{key_pressed, Key, KEYPAD_CHANNEL};
 pub use usb::init as init_usb;
 pub use usb::{HidEvent, HID_CHANNEL};
+pub use watchdog::init as init_watchdog;
