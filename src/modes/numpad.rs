965c62cb2390a8f5ba568b33a8f0da4e5b01730e
Bas Bieling <bas@scrumble.nl>
2026-02-05 22:54:19 +0100

    Attempt at LED indicators

--- src/modes/numpad.rs
+++ src/modes/numpad.rs
@@ -1,9 +1,10 @@
 use crate::modes::{Mode, MODE_RUNNING};
-use crate::show_text;
-use crate::tasks::{HID_CHANNEL, HidEvent, KEYPAD_CHANNEL, Key};
+use crate::tasks::{LED_STATE, HID_CHANNEL, HidEvent, KEYPAD_CHANNEL, Key, KeyboardLed, DisplayProxy};
 use portable_atomic::{Ordering};
 use embassy_sync::pubsub::{WaitResult};
-use core::fmt::Write;
+use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::geometry::{Point, Size};
+use embedded_graphics::pixelcolor::BinaryColor;
 
 pub struct NumpadMode {
 }
@@ -13,11 +14,46 @@ impl NumpadMode {
         Self {
         }
     }
+
+    fn draw_leds(&self, display: &mut DisplayProxy) {
+        const LED_BOX_HEIGHT: u16 = 10;
+        const LED_BOX_WIDTH: u16 = 20;
+        const LED_BOX_SPACING: i32 = 5;
+        const LED_BOX_THICKNESS: u32 = 2;
+        const LED_VALUES: [KeyboardLed; 3] = [KeyboardLed::NumLock, KeyboardLed::CapsLock, KeyboardLed::ScrollLock];
+
+        display.clear(BinaryColor::Off).unwrap();
+
+        for (i, led) in LED_VALUES.iter().enumerate() {
+            let offset_x = LED_BOX_SPACING + (i as i32 * (LED_BOX_WIDTH as i32 + LED_BOX_SPACING));
+
+            display.fill_solid(
+                &embedded_graphics::primitives::Rectangle::new(
+                    Point::new(offset_x, 60),
+                    Size::new(LED_BOX_WIDTH as u32, LED_BOX_HEIGHT as u32),
+                ),
+                BinaryColor::On,
+            ).unwrap();
+
+            if !LED_STATE.test(*led) {
+                display.fill_solid(
+                    &embedded_graphics::primitives::Rectangle::new(
+                        Point::new(offset_x + LED_BOX_THICKNESS as i32, 21),
+                        Size::new(LED_BOX_WIDTH as u32 - LED_BOX_THICKNESS * 2, LED_BOX_HEIGHT as u32 - LED_BOX_THICKNESS * 2),
+                    ),
+                    BinaryColor::Off,
+                ).unwrap();
+            }
+        }
+
+        display.flush().unwrap();
+    }
 }
 
 impl Mode for NumpadMode {
     async fn task(&mut self) {
         let mut keypad = KEYPAD_CHANNEL.subscriber().unwrap();
+        let mut display = DisplayProxy::new();
         let hid = HID_CHANNEL.sender();
 
         hid.send(HidEvent::Reset).await;
@@ -35,20 +71,9 @@ impl Mode for NumpadMode {
                         hid.send(HidEvent::ReleaseKey(keycode)).await;
                     }
                 }
-
-                let mut str = heapless::String::<32>::new();
-
-                if let Ok(()) = write!(
-                    &mut str,
-                    "{} {:?}",
-                    if event.pressed { "PRESS " } else { "RELEASE " },
-                    event.key
-                ) {
-                    show_text(&str);
-                } else {
-                    show_text("Whoops");
-                }
             }
+
+            self.draw_leds(&mut display);
         }
     }
 }
--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -10,5 +10,5 @@ pub use hotkeys::init as init_hotkeys;
 pub use keypad::init as init_keypad;
 pub use keypad::{key_pressed, Key, KEYPAD_CHANNEL};
 pub use usb::init as init_usb;
-pub use usb::{HidEvent, HID_CHANNEL};
+pub use usb::{HidEvent, HID_CHANNEL, LED_STATE, KeyboardLed};
 pub use watchdog::init as init_watchdog;
